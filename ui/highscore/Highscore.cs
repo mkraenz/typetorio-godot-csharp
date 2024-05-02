656e543ca3b6077aa617da8b7009dc172c555764
Mirco Kraenz <software@kraenz.eu>
2024-05-02 10:02:28 +0200

    feat: score with leading zeros

--- ui/highscore/Highscore.cs
+++ ui/highscore/Highscore.cs
@@ -13,11 +13,17 @@ public partial class Highscore : MarginContainer
 		labels = GetNode<MyScore>("MyScore");
 
 		eventbus.WordCleared += _OnWordCleared;
+		_UpdateLabel();
 	}
 
 	private void _OnWordCleared(string word)
 	{
 		_score++;
-		labels.Value = _score.ToString();
+		_UpdateLabel();
+	}
+
+	private void _UpdateLabel()
+	{
+		labels.Value = _score.ToString("D10"); // 10 digits with leading zeros
 	}
 }
