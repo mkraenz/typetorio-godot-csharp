b34d4fa2d3ca2c16fc0df1ae2575081d7ae96d5e
Mirco Kraenz <software@kraenz.eu>
2024-05-05 20:16:03 +0200

    refactor:figure 8 component works as originally designed via export variable

--- effects/figure-eight-path/MoveInFigureEight.cs
+++ effects/figure-eight-path/MoveInFigureEight.cs
@@ -3,31 +3,19 @@ using System;
 
 public partial class MoveInFigureEight : Path2D
 {
-	private NodePath _movedObject;
 
-	// [Export]
-	// public NodePath MovedObject
-	// {
-	// 	get => _movedObject;
-	// 	set
-	// 	{
-			// _movedObject = value;
-			// var remoteTransform = GetNode<RemoteTransform2D>("PathFollow2D/RemoteTransform2D");
-			// // var nodePath = new NodePath($"../../{_movedObject}");
-			// var x = GetNode(_movedObject.ToString());
-			// remoteTransform.RemotePath = _movedObject; // _movedObject is relative to MoveInFigureEight, but needs to be relative to the RemoteTransform2D to work properly
-			// remoteTransform.ForceUpdateCache();
-	// 	}
-	// }
-
+	[Export]
+	public NodePath _movedObject;
 
 	public override void _Ready()
 	{
-		// if (MovedObject is null) throw new Exception("_movedObject not set via GD Editor");
+		if (_movedObject is null) throw new Exception("_movedObject not set via GD Editor");
 
-		// var remoteTransform = GetNode<RemoteTransform2D>("PathFollow2D/RemoteTransform2D");
-		// // var nodePath = new NodePath($"../../{_movedObject}");
-		// var x = GetNode(MovedObject.ToString());
-		// remoteTransform.RemotePath = MovedObject; // _movedObject is relative to MoveInFigureEight, but needs to be relative to the RemoteTransform2D to work properly
+		var remoteTransform = GetNode<RemoteTransform2D>("PathFollow2D/RemoteTransform2D");
+
+		// _movedObject is relative to MoveInFigureEight, but needs to be relative to the RemoteTransform2D to work properly
+		var movedObjectNode = GetNode(_movedObject);
+		var path = remoteTransform.GetPathTo(movedObjectNode); // I tried all sorts of ways of getting the path but this is the only one that works
+		remoteTransform.RemotePath = path;
 	}
 }
