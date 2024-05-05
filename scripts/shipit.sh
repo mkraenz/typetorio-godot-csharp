e4e5b5faba2f51d9c85d4c19bd9cda2c70bac661
Mirco Kraenz <software@kraenz.eu>
2024-05-05 11:36:37 +0200

    chore: wip: windows export

--- scripts/shipit.sh
+++ scripts/shipit.sh
@@ -9,6 +9,6 @@ mkdir -p .build/linux
 /home/mirco/programming/gamedev/itchio-butler/butler push .build/linux mkraenz/typetorio:linux
 
 # windows TODO
-# mkdir -p .build/linux
-# /home/mirco/programming/gamedev/godot4/Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64 --export-release 'Windows' .build/windows/Typetorio.x86_64
+# mkdir -p .build/windows
+# /home/mirco/programming/gamedev/godot4/Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64 --export-release 'Windows Desktop' .build/windows/Typetorio.x86_64
 # /home/mirco/programming/gamedev/itchio-butler/butler push .build/windows mkraenz/typetorio:windows
