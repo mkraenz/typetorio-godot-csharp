5bf5a847f6a66026dfb7ae33c6db0084a925afa9
Mirco Kraenz <software@kraenz.eu>
2024-05-05 11:33:51 +0200

    chore: release script for linux version

---
+++ scripts/shipit.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+rm -rf .build
+mkdir -p .build/linux
+
+/home/mirco/programming/gamedev/godot4/Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64 --export-release 'Linux/X11' .build/linux/Typetorio.x86_64
+
+/home/mirco/programming/gamedev/itchio-butler/butler push .build/linux mkraenz/typetorio:linux
+
+# windows TODO
+# mkdir -p .build/linux
+# /home/mirco/programming/gamedev/godot4/Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64 --export-release 'Windows' .build/windows/Typetorio.x86_64
+# /home/mirco/programming/gamedev/itchio-butler/butler push .build/windows mkraenz/typetorio:windows
