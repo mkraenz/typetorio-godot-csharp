f358b41098cd119e8dee42d9b286d082e9f02c09
Mirco Kraenz <software@kraenz.eu>
2024-05-05 11:08:39 +0200

    feat: as combo increases fontsize, color and rainbowiness increase

--- ui/combometer/ComboMeter.cs
+++ ui/combometer/ComboMeter.cs
@@ -22,5 +22,36 @@ public partial class ComboMeter : MarginContainer
 		float displayedMultiplier = multiplier == 0 ? 1 : multiplier;
 		string val = ((int)displayedMultiplier).ToString();
 		labels.Value = $"x{val}";
+		labels.Color = MultiplierColor(displayedMultiplier);
+		labels.ValueFontSize = MultiplierFontSize(displayedMultiplier);
+		labels.RainbowEnabled = displayedMultiplier >= 50;
 	}
+
+	int MultiplierFontSize(float displayedMultiplier) =>
+		displayedMultiplier switch
+		{
+			> 49 => 48,
+			> 24 => 32,
+			> 16 => 28,
+			> 10 => 24,
+			> 6 => 20,
+			> 3 => 18,
+			_ => 16,
+		};
+
+	/// <summary>
+	/// Relational Pattern matching https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/pattern-matching#relational-patterns */
+	/// </summary>
+	string MultiplierColor(float displayedMultiplier) =>
+		displayedMultiplier switch
+		{
+			> 24 => "red",
+			> 16 => "orange",
+			> 10 => "yellow",
+			> 6 => "green",
+			> 3 => "blue",
+			_ => "white",
+		};
+
+
 }
--- ui/score/MyScore.cs
+++ ui/score/MyScore.cs
@@ -5,13 +5,16 @@ using System;
 public partial class MyScore : MarginContainer
 {
 	private string _value = "";
-	private string _label = "";
+	private string _labelText = "";
+	private string _color = "white";
+	private int _valueFontSize = 16;
+	private bool _rainbowEnabled = false;
 
 	[ExportCategory("Scoring")]
 	[Export]
 	public string Value
 	{
-		get { return _value; }
+		get => _value;
 		set
 		{
 			_value = value;
@@ -22,35 +25,79 @@ public partial class MyScore : MarginContainer
 	[Export]
 	public string Label
 	{
-		get => _label;
+		get => _labelText;
 		set
 		{
-			_label = value;
+			_labelText = value;
 			_UpdateAllLabels();
 		}
 	}
 
-	private Label valueLabel;
-	private Label label;
+	[Export]
+	public string Color
+	{
+		get => _color;
+		set
+		{
+			_color = value;
+			_UpdateAllLabels();
+		}
+	}
+
+	[Export]
+	public int ValueFontSize
+	{
+		get => _valueFontSize;
+		set
+		{
+			_valueFontSize = value;
+			_UpdateAllLabels();
+		}
+	}
+
+	[Export]
+	public bool RainbowEnabled
+	{
+		get => _rainbowEnabled;
+		set
+		{
+			_rainbowEnabled = value;
+			_UpdateAllLabels();
+		}
+	}
+
+	private RichTextLabel _valueLabel;
+	private Label _label;
 
 	public override void _Ready()
 	{
-		label = GetNode<Label>("H/Label");
-		valueLabel = GetNode<Label>("H/Value");
+		_label = GetNode<Label>("H/Label");
+		_valueLabel = GetNode<RichTextLabel>("H/Value");
+		ValueFontSize = GetThemeDefaultFontSize();
 		_UpdateAllLabels();
 
 	}
 
 	private void _UpdateAllLabels()
 	{
-		if (valueLabel != null)
+		if (_valueLabel != null)
 		{
-			valueLabel.Text = _value;
+			string rainBowFrequency = RainbowEnabled ? "1.0" : "0.0";
+			// rainbow and color are not compatible
+			if (RainbowEnabled)
+			{
+				_valueLabel.Text = $"[rainbow freq={rainBowFrequency} sat=0.8 val=0.8][font_size={ValueFontSize}]{Value}[/font_size][/rainbow]";
+			}
+			else
+			{
+				_valueLabel.Text = $"[font_size={ValueFontSize}][color={Color}]{Value}[/color][/font_size]";
+
+			}
 		}
 
-		if (label != null)
+		if (_label != null)
 		{
-			label.Text = _label;
+			_label.Text = _labelText;
 		}
 	}
 
