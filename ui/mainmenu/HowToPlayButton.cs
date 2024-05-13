70735b7c881831bcff50807f7d6c791f4b56dea5
Mirco Kraenz <software@kraenz.eu>
2024-05-13 09:12:18 +0200

    fix: svg icons looked ugly

--- ui/mainmenu/HowToPlayButton.cs
+++ ui/mainmenu/HowToPlayButton.cs
@@ -2,7 +2,6 @@ using System;
 using Globals;
 using Godot;
 
-// TODO icon looks ugly as svg. the question mark is not fully rendered. fix it. maybe need to use png?
 namespace UI
 {
     public partial class HowToPlayButton : Button
