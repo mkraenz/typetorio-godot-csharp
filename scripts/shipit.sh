9dce87d2d07ce2e72fb7136851a3bdd38a94a4af
Mirco Kraenz <software@kraenz.eu>
2024-05-14 20:18:23 +0200

    build: windows build to itchio

--- scripts/shipit.sh
+++ scripts/shipit.sh
@@ -8,7 +8,6 @@ mkdir -p .build/linux
 
 /home/mirco/programming/gamedev/itchio-butler/butler push .build/linux mkraenz/typetorio:linux
 
-# windows TODO
-# mkdir -p .build/windows
-# /home/mirco/programming/gamedev/godot4/Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64 --export-release 'Windows Desktop' .build/windows/Typetorio.x86_64
-# /home/mirco/programming/gamedev/itchio-butler/butler push .build/windows mkraenz/typetorio:windows
+mkdir -p .build/windows
+/home/mirco/programming/gamedev/godot4/Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64 --export-release 'Windows Desktop' .build/windows/Typetorio.x86_64
+/home/mirco/programming/gamedev/itchio-butler/butler push .build/windows mkraenz/typetorio:windows
